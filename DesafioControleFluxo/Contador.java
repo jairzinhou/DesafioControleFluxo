30556742a8e76b81849ac83c3a9dda7182b55a5a
jairzinhou <jairaraujo209@gmail.com>
2023-09-05 21:18:42 -0300

    o

--- DesafioControleFluxo/Contador.java
+++ DesafioControleFluxo/Contador.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
+        System.out.println("digite o primeiro parâmetro");
         int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
+        System.out.println("digite o segundo parâmetro");
         int parametroDois = terminal.nextInt();
 
         try {
